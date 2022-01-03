030da917e69a33cce9611eb136ba240862250b5f
Kendrick Webster <kw80301@gmail.com>
2022-01-02 17:02:47 -0700

    add fake todos

--- src/app/controllers/api.controller.ts
+++ src/app/controllers/api.controller.ts
@@ -1,10 +1,14 @@
-import { Context, Get, HttpResponseOK } from '@foal/core';
+import { Get, HttpResponseOK } from '@foal/core';
 
 export class ApiController {
 
-  @Get('/')
-  index(ctx: Context) {
-    return new HttpResponseOK('Hello world!');
+  @Get('/todos')
+  getTodos() {
+    const todos = [
+      { id: 1, text: 'My task 1' },
+      { id: 2, text: 'My task 2' }
+    ];
+    return new HttpResponseOK(todos);
   }
 
 }
